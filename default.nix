dd41a13f5081e8d5914329209a074b20360e2141
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-02 16:27:56 +0300

    Add flake.

--- default.nix
+++ default.nix
@@ -1,31 +1,13 @@
-{ nixpkgs ? (import <nixpkgs> { }).fetchgit {
-  url = "https://github.com/NixOS/nixpkgs.git";
-  rev = "ce9f1aaa39ee2a5b76a9c9580c859a74de65ead5";
-  sha256 = "1s2b9rvpyamiagvpl5cggdb2nmx4f7lpylipd397wz8f0wngygpi";
-}, overlayUrl ? "git@gitlab.intr:_ci/nixpkgs.git", overlayRef ? "master" }:
+{ nixpkgs }:
 
-with import nixpkgs {
-  overlays = [
-    (self: super:
-      with super; rec {
-        overlayed = (import nixpkgs {
-          overlays = [
-            (import (builtins.fetchGit {
-              url = "https://gitlab.intr/_ci/nixpkgs";
-              ref = "master";
-            }))
-          ];
-        });
-      })
-  ];
-};
+with nixpkgs;
 
 let
 
   inherit (builtins) toJSON;
   inherit (dockerTools) buildLayeredImage;
   inherit (lib) concatMapStringsSep;
-  inherit (overlayed.lib) dockerRunCmd flattenSet mkRootfs;
+  inherit (lib) dockerRunCmd flattenSet mkRootfs;
 
   rootfs = mkRootfs {
     name = "rsyslog-rootfs";
